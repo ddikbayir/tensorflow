c2bf60d5cd5e27a4a5b7a142d98e42e92fb1002d
Dikbayir D <dogadikbay@equinox.ftpn.ornl.gov>
2019-07-13 11:32:30 -0400

    Added custom placer

--- tensorflow/core/common_runtime/placer.cc
+++ tensorflow/core/common_runtime/placer.cc
@@ -1,11 +1,8 @@
 /* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,8 +13,14 @@ limitations under the License.
 #include "tensorflow/core/common_runtime/placer.h"
 
 #include <memory>
+#include <set>
+#include <utility>
 #include <vector>
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
 
+#include "absl/strings/str_join.h"
 #include "tensorflow/core/common_runtime/colocation_graph.h"
 #include "tensorflow/core/common_runtime/device.h"
 #include "tensorflow/core/framework/attr_value_util.h"
@@ -25,12 +28,20 @@ limitations under the License.
 #include "tensorflow/core/framework/function.h"
 #include "tensorflow/core/framework/graph.pb.h"
 #include "tensorflow/core/framework/node_def_util.h"
+#include "tensorflow/core/framework/kernel_def_util.h"
+#include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/types.h"
 #include "tensorflow/core/framework/types.pb.h"
 #include "tensorflow/core/lib/core/errors.h"
+#include "tensorflow/core/lib/core/stringpiece.h"
+#include "tensorflow/core/lib/strings/str_util.h"
+#include "tensorflow/core/lib/strings/strcat.h"
+#include "tensorflow/core/util/device_name_utils.h"
 #include "tensorflow/core/util/dump_graph.h"
 #include "tensorflow/core/util/port.h"
 
+#include <fstream>
+using namespace std;
 namespace tensorflow {
 
 namespace {
@@ -41,11 +52,12 @@ namespace {
 // TODO(vrv): Currently this handles only nodes with one output, but
 // this could be extended to handle the case where a node has many
 // outputs that are connected to nodes in the same colocation group.
+/*
 bool IsGeneratorNode(const Node* node) {
   return node->num_inputs() == 0 && node->num_outputs() == 1 &&
          !IsRefType(node->output_type(0));
 }
-
+*/
 void LogDeviceAssignment(const Node* node, bool log_device_placement) {
   // Log placement if log_device_placement is set.
   if (log_device_placement) {
@@ -63,7 +75,8 @@ Status AssignAndLog(int assigned_device, Node* node,
   node->set_assigned_device_name_index(assigned_device);
 
   // Constraint the group of node to the assigned device.
-  TF_RETURN_IF_ERROR(colocation_graph->LimitToAssignedDevice(*node));
+  //We disable this to disable placer algo (DOGA)
+  //TF_RETURN_IF_ERROR(colocation_graph->LimitToAssignedDevice(*node));
 
   LogDeviceAssignment(node, log_device_placement);
   return Status::OK();
@@ -95,6 +108,32 @@ Placer::Placer(Graph* graph, const string& function_name,
 
 Placer::~Placer() {}
 
+/**
+/* Helper function to identify if the target operation has a GPU implementation
+*/
+int HasGPUKernel(Node* node) {
+  KernelList krns = GetRegisteredKernelsForOp(node->type_string());
+  int res = 0;
+  AttrSlice as = node->attrs();
+  cout << "DEBUG:" << SummarizeAttrs(node->def()) << endl;
+  string attr_sum = SummarizeAttrs(node->def());
+  
+  char * cstr = new char [attr_sum.length()+1 ];
+  strcpy(cstr, attr_sum.c_str());
+
+  bool match = false;
+  for(const auto& kernel_def : krns.kernel()) {
+    if(kernel_def.device_type().compare("GPU") == 0) {
+       KernelAttrsMatch(kernel_def, as, &match);
+      if(match) {
+        res = 1;
+        break;
+      }
+    } 
+  }
+  return res;
+}
+//DOGA: Alternate placer for custom placement
 Status Placer::Run() {
   if (devices_->devices().empty()) {
     return errors::FailedPrecondition("No devices are registered");
@@ -110,128 +149,46 @@ Status Placer::Run() {
               << node->assigned_device_name() << "'";
     }
   }
-
   FunctionStack stack(function_name_);
   ColocationGraph colocation_graph(graph_, stack, flib_def_, devices_,
                                    default_device_, allow_soft_placement_,
                                    log_device_placement_);
 
+
   TF_RETURN_IF_ERROR(colocation_graph.Initialize());
+  cout << "Reading placement file..." << endl; 
+  std::unordered_map<string, int> parcore_placement;
+  
+  
+  std::ifstream placement_file("/home/dogadikbay/placement.place");
+  cout << "Read placement file..." << endl;
 
-  // For each node, assign a device based on the constraints in the disjoint
-  // node set.
-  std::vector<Node*> second_pass;
-  for (Node* node : graph_->op_nodes()) {
-    // The graph may have come pre-populated by the framework with assigned
-    // devices (e.g., for stateful placements), so the placer should not try to
-    // place nodes that are already placed.
-    if (node->has_assigned_device_name()) {
-      TF_RETURN_IF_ERROR(colocation_graph.LimitToAssignedDevice(*node));
-      LogDeviceAssignment(node, log_device_placement_);
-      continue;
-    }
-
-    // Heuristic A: prefer to place "generators" with their only
-    // consumers.
-    //
-    // If this is a node with no inputs and one output, we save
-    // this for a second pass, so that the consumer's placement
-    // is chosen.
-    if (IsGeneratorNode(node)) {
-      second_pass.push_back(node);
-      continue;
-    }
-
-    const std::vector<Device*>* devices;
-    Status status = colocation_graph.GetDevicesForNode(node, &devices);
-    if (!status.ok()) {
-      return AttachDef(
-          errors::InvalidArgument("Cannot assign a device for operation ",
-                                  node->name(), ": ", status.error_message()),
-          *node);
-    }
-
-    // Returns the first device in sorted devices list so we will always
-    // choose the same device.
-    //
-    // TODO(vrv): Factor this assignment out into a pluggable
-    // algorithm, so that Placer is responsible for enforcing
-    // preconditions and we can experiment with other algorithms when
-    // given a choice of devices. Once we have a better idea of the
-    // types of heuristics we want to use and the information needed
-    // to perform good placement we can add an interface for this.
-    int assigned_device = -1;
-
-    // Heuristic B: If the node only operates on metadata, not data,
-    // then it is desirable to place that metadata node with its
-    // input.
-    if (IsMetadata(node)) {
-      // Make sure that the input device type is in the list of supported
-      // device types for this node.
-      const Node* input = (*node->in_edges().begin())->src();
-      // TODO(vrv): if the input is empty, consider postponing this
-      // node's assignment to the second pass, so that we handle the
-      // case where a metadata node's input comes from a backedge
-      // of a loop.
-      if (CanAssignToDevice(input->assigned_device_name(), *devices)) {
-        assigned_device = input->assigned_device_name_index();
-      }
-    }
-
-    // Provide the default, if necessary.
-    if (assigned_device == -1) {
-      assigned_device = graph_->InternDeviceName((*devices)[0]->name());
-    }
-
-    TF_RETURN_IF_ERROR(AssignAndLog(assigned_device, node, &colocation_graph,
-                                    log_device_placement_));
+  string node_name;
+  int placed_dev;
+  int counter= 0;
+  //map nodes to assigned devices
+  while(placement_file >> node_name >> placed_dev) {
+    parcore_placement[node_name] = placed_dev;
   }
+  cout << "Populated unordered placement map..." << endl;
+  std::vector<Device*> devs = devices_->devices();
+  for(Node* node : graph_->op_nodes()) {
+    int assigned_device = 0;
+    int assigned_dev_id = 0;
+    
+    //Assign the node
+    assigned_dev_id = parcore_placement[node->name()] + 1;
+    cout << "Node: " << node->name() << endl;
+    cout << "Node ID: " << assigned_dev_id << endl;
 
-  // Perform a second pass assignment for those nodes explicitly
-  // skipped during the first pass.
-  for (Node* node : second_pass) {
-    const std::vector<Device*>* devices;
-    Status status = colocation_graph.GetDevicesForNode(node, &devices);
-    if (!status.ok()) {
-      return AttachDef(
-          errors::InvalidArgument("Cannot assign a device for operation ",
-                                  node->name(), ": ", status.error_message()),
-          *node);
-    }
-
-    int assigned_device = -1;
-
-    // Heuristic A application.
-    if (IsGeneratorNode(node) && !node->out_edges().empty()) {
-      const Node* output = (*node->out_edges().begin())->dst();
-      int output_device_name = output->assigned_device_name_index();
-
-      const bool consumers_on_same_device = std::all_of(
-          node->out_edges().begin(), node->out_edges().end(),
-          [output_device_name](const Edge* e) {
-            return e->dst()->assigned_device_name_index() == output_device_name;
-          });
-
-      if (consumers_on_same_device &&
-          CanAssignToDevice(output->assigned_device_name(), *devices)) {
-        assigned_device = output_device_name;
-      }
-    }
-
-    // Provide the default, if necessary.
-    if (assigned_device == -1) {
-      assigned_device = graph_->InternDeviceName((*devices)[0]->name());
-    }
-
-    TF_RETURN_IF_ERROR(AssignAndLog(assigned_device, node, &colocation_graph,
-                                    log_device_placement_));
+    assigned_device = graph_->InternDeviceName(devs[assigned_dev_id]->name());
+    cout << "Assigned device created: " << assigned_device << endl;
+    TF_RETURN_IF_ERROR(AssignAndLog(assigned_device, node, &colocation_graph, log_device_placement_)); 
   }
-
-  if (VLOG_IS_ON(3)) {
-    DumpGraphToFile("placer_output", *graph_, nullptr);
-  }
+  cout << "Finished device assignment without any errors..." << endl;
   return Status::OK();
 }
+
 
 bool Placer::CanAssignToDevice(const string& candidate_device_name,
                                const std::vector<Device*>& devices) const {
